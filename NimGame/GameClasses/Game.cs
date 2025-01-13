1aeee4458f6dc6132968a67b42839aef6451332f
Aiden Sanders <aidensandersbiz@gmail.com>
2025-01-13 09:50:05 -0700

    Base Game, Board, & Player classes

---
+++ NimGame/GameClasses/Game.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimGame.GameClasses
+{
+    public class Game
+    {
+        public string? Winner { get; private set; }
+
+        public Game(GameDifficulty difficulty, string player1Name, string player2Name = "CPU", bool isPvC = false, Players.CPUDifficulty cpu_difficulty = Players.CPUDifficulty.MEDIUM)
+        {
+            gameDifficulty = difficulty;
+            gameBoard = new Board((int)gameDifficulty);
+
+            players[0] = new Players.Player(player1Name);
+            if (isPvC)
+            {
+                this.isPvC = true;
+                players[1] = new Players.CPU(cpu_difficulty);
+            }
+            else
+            {
+                players[1] = new Players.Player(player2Name);
+            }
+
+            Random random = new Random();
+            currentPlayer = random.Next(2);
+        }
+
+        public void UpdateBoard(int row)
+        {
+            gameBoard.UpdateRow(row);
+            playerActed = true;
+        }
+
+        public void SwitchPlayer()
+        {
+            if (playerActed)
+            {
+                currentPlayer = (currentPlayer == 0) ? 1 : 0;
+                playerActed = false;
+                if (isPvC && currentPlayer == 1)
+                {
+                    players[1].TakeTurn(ref gameBoard);
+                }
+            }
+        }
+
+        public bool CheckWinner()
+        {
+            if (gameBoard.IsEmpty())
+            {
+                Winner = (currentPlayer == 0) ? players[1].PlayerName : players[0].PlayerName;
+                return true;
+            }
+
+            return false;
+        }
+
+        private Players.Player[] players = new Players.Player[2];
+        private bool isPvC = false;
+        private Board gameBoard;
+        private readonly GameDifficulty gameDifficulty;
+
+        private int currentPlayer;
+        private bool playerActed = false;
+    }
+
+    public enum GameDifficulty
+    {
+        EASY = 3,
+        MEDIUM,
+        HARD
+    }
+
+    public class Board
+    {
+        public Board(int rowCount)
+        {
+            Rows = new int[rowCount];
+        }
+
+        public void UpdateRow(int row)
+        {
+            if ((row == selectedRow || selectedRow == -1) && Rows[row] > 0)
+            {
+                selectedRow = row;
+                Rows[row]--;
+            }
+        }
+
+        public void DeselectRow() { selectedRow = -1; }
+
+        public bool IsEmpty()
+        {
+            foreach (var rowCount in Rows)
+            {
+                if (rowCount > 0) return false;
+            }
+
+            return true;
+        }
+
+        public int[] Rows { get; private set; }
+        private int selectedRow = -1;
+    }
+}
---
+++ NimGame/GameClasses/Players.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimGame.GameClasses.Players
+{
+    public class Player
+    {
+        public string PlayerName { get; protected set; }
+
+        public Player(string playerName)
+        {
+            PlayerName = playerName;
+        }
+
+        virtual public void TakeTurn(ref Board board) { }
+    }
+
+    public class CPU : Player
+    {
+        public CPU(CPUDifficulty cpu_difficulty) : base("CPU") 
+        {
+            difficulty = cpu_difficulty;
+
+            switch (difficulty)
+            {
+                case CPUDifficulty.EASY:
+                    PlayerName = "Easy CPU";
+                    break;
+                case CPUDifficulty.MEDIUM:
+                    PlayerName = "Medium CPU";
+                    break;
+                case CPUDifficulty.HARD:
+                    PlayerName = "Hard CPU";
+                    break;
+                default:
+                    PlayerName = "CPU";
+                    break;
+            }
+        }
+
+        override public void TakeTurn(ref Board board)
+        {
+            switch (difficulty)
+            {
+                case CPUDifficulty.EASY:
+                    break;
+                case CPUDifficulty.MEDIUM:
+                    break;
+                case CPUDifficulty.HARD:
+                    foreach (var rowCount in board.Rows) 
+                    {
+                        if ((rowCount & 0b0001) != 0)
+                        {
+                            nimSumCounts[3]++;
+                        }
+                        if ((rowCount & 0b0010) != 0)
+                        {
+                            nimSumCounts[2]++;
+                        }
+                        if ((rowCount & 0b0100) != 0)
+                        {
+                            nimSumCounts[1]++;
+                        }
+                        if ((rowCount & 0b1000) != 0)
+                        {
+                            nimSumCounts[0]++;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private CPUDifficulty difficulty;
+
+        private Random rand = new Random();
+        private int[] nimSumCounts = new int[4];
+        private bool nimSumAchievable = false;
+    }
+
+    public enum CPUDifficulty
+    {
+        EASY,
+        MEDIUM,
+        HARD
+    }
+}
