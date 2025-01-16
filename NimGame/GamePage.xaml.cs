3a8cc0061f32c6682547c20b511a27571009e7a9
ctsosie115 <157407430+ctsosie115@users.noreply.github.com>
2025-01-15 17:33:42 -0700

    Update GamePage.xaml.cs

--- NimGame/GamePage.xaml.cs
+++ NimGame/GamePage.xaml.cs
@@ -6,6 +6,30 @@ public partial class GamePage : ContentPage
 	{
 		InitializeComponent();
 	}
+    public GamePage(Game game)
+    {
+        InitializeComponent();
+
+        this.game = game;
+
+        var player1Label = FindByName("Player1Name") as Label;
+        if (player1Label != null)
+        {
+            player1Label.Text = this.game.GetPlayerName(0);
+        }
+
+        var player2Label = FindByName("Player2Name") as Label;
+        if (player2Label != null)
+        {
+            player2Label.Text = this.game.GetPlayerName(1);
+        }
+
+        var currentPlayerLabel = FindByName("CurrentPlayerName") as Label;
+        if (currentPlayerLabel != null)
+        {
+            currentPlayerLabel.Text = this.game.GetCurrentPlayerName() + "'s Turn";
+        }
+    }
 
     private void HowToPlay(object sender, EventArgs e)
     {
@@ -23,34 +47,70 @@ public partial class GamePage : ContentPage
     }
     private void NewGame(object sender, EventArgs e)
     {
+<<<<<<< Updated upstream
+=======
+        this.game = new Game(GameDifficulty.MEDIUM, "HI");
+>>>>>>> Stashed changes
     }
 
     private void ExitGame(object sender, EventArgs e)
     {
+        App.Current.Quit();
     }
 
     private void PassTurn(object sender, EventArgs e)
+<<<<<<< Updated upstream
     { 
+=======
+    {
+        this.game.SwitchPlayer();
+
+        var currentPlayerLabel = FindByName("CurrentPlayerName") as Label;
+        if (currentPlayerLabel != null)
+        {
+            currentPlayerLabel.Text = this.game.GetCurrentPlayerName() + "'s Turn";
+        }
+>>>>>>> Stashed changes
     }
 
     private void Row1(object sender, EventArgs e)
     {
+<<<<<<< Updated upstream
+=======
+        this.game.UpdateBoard(0);
+>>>>>>> Stashed changes
     }
 
     private void Row2(object sender, EventArgs e)
     {
+<<<<<<< Updated upstream
+=======
+        this.game.UpdateBoard(1);
+>>>>>>> Stashed changes
     }
 
     private void Row3(object sender, EventArgs e)
     {
+<<<<<<< Updated upstream
+=======
+        this.game.UpdateBoard(2);
+>>>>>>> Stashed changes
     }
 
     private void Row4(object sender, EventArgs e)
     {
+<<<<<<< Updated upstream
+=======
+        this.game.UpdateBoard(3);
+>>>>>>> Stashed changes
     }
 
     private void Row5(object sender, EventArgs e)
     {
+<<<<<<< Updated upstream
+=======
+        this.game.UpdateBoard(4);
+>>>>>>> Stashed changes
     }
 
 }
